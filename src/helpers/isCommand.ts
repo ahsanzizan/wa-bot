95caa837a039afb3a20c6c005a86a15b71a4f6c7
Ahsan <ahsanaz461@gmail.com>
2024-02-13 18:07:06 +0700

    refactor: FIx mistyping

--- src/helpers/isCommand.ts
+++ src/helpers/isCommand.ts
@@ -3,7 +3,7 @@ import commands from "../utils/userCommands";
 /**
  * Decrypts a quoted message containing media and returns the base64 of the image.
  * Also saves the decrypted media as a file.
- * @param {Message} message The message received from sender.
+ * @param {string} message The message received from sender.
  */
 const isCommand = (message: string) => {
   return Boolean(Object.keys(commands).indexOf(message));
