aef6935a328a9103ba518c252ebdd95d62157bde
Ahsan <ahsanaz461@gmail.com>
2024-02-14 09:38:19 +0700

    fix: Expect ts eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 import eslint from "eslint";
+// @ts-expect-error: The typescript-eslint doesn't exist (idk how to fix that yet)
 import tseslint from "typescript-eslint";
 
 export default tseslint.config();
